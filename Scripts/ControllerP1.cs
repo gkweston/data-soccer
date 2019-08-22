91afcaf590de418ef81b62613d0098dcc3377f08
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-08-22 18:49:49 -0500

    Update ControllerP1.cs

--- Scripts/ControllerP1.cs
+++ Scripts/ControllerP1.cs
@@ -2,23 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// tracks ball at speed set in Unity engine
+// tracks ball at speed set in Unity engine then shoots when in shooting radius of goal
+// conditionals & booleans need consolidation
 
 public class ControllerP1 : MonoBehaviour
 {
-    private Transform _player1Trans;
-    public Transform targetBall;
+    public Transform ballTrans;
 
+    private bool _hasBall;
+    
     public float speed;
+    private Vector2 _goalLine;
+    private Vector2 _startingPosition;
+    private float _distanceToGoal;
+    private float _distanceToBall;
+    public float shotRadius;
+    
+    private bool _takeShot;
+    
     
     void Start()
     {
+        _hasBall = false;
+        _goalLine.x = 0;
+        _goalLine.y = 5.5f;
         
     }
 
     void Update()
     {
+        _distanceToBall = Vector2.Distance(transform.position, ballTrans.position);
+        _distanceToGoal = Vector2.Distance(transform.position, _goalLine);
+
+        if (_distanceToBall <= .25f && !_takeShot)
+        {
+            _hasBall = true;
+        }
+        else
+        {
+            _hasBall = false;
+        }
+
         float step = speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, targetBall.position, step);
+        
+        if (_hasBall && !_takeShot)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _goalLine, step);
+        }
+        else
+        {
+            if (!_takeShot)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, ballTrans.position, step);
+            }
+        }
+
+        if (_distanceToGoal < shotRadius)
+        {
+            _takeShot = true;
+        }
+        
+        if (_takeShot)
+        {
+            _hasBall = false;
+        }
+        
     }
 }
