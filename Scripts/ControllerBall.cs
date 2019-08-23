8f9cc0cf6fb3b4a3442fb0e4ca2b2c9a27e5be35
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-08-23 15:15:38 -0500

    Update ControllerBall.cs

--- Scripts/ControllerBall.cs
+++ Scripts/ControllerBall.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 // transforms the ball continuously in a figure 8 to test player controller scripts
-// conditions & booleans need consolidation
 
 public class ControllerBall : MonoBehaviour
 {
-    public float rotateSpeed = 5f;
+    public float rotateSpeed = 5f;    // only necessary to make ball move in figure 8, which is useful for testing player script
     public float shotSpeed;
+    private float _angle;
     public float radius = 0.1f;
-    public Transform player1Trans;
+    private Vector2 _center;    // alias for ball position set in Unity, origin of figure 8
+    
+    public Transform player1Trans;    // allows player position to be shared with ball
     private bool _hasBall;
     
-    private Vector2 _center;
-    private Vector2 _goalLine;
-    private float _angle;
-    public float shotRadius;
+
+    private Vector2 _goalLine;    // defines goal position on field
+
+    public float shotRadius;    // allows shot radius to be changed and tested in Unity
 
     private float _distanceToBall;
     private float _distanceToGoal;
     private bool _takeShot;
     
     
-    void Start()
+    void Start()    // assign center of figure 8, default the player to not having possession, define goal position
     {
         _center = transform.position;
         _hasBall = false;
@@ -35,27 +37,27 @@ public class ControllerBall : MonoBehaviour
 
     void Update()
     {
-        _distanceToBall = Vector2.Distance(transform.position, player1Trans.position);
+        _distanceToBall = Vector2.Distance(transform.position, player1Trans.position);    // update player distances
         _distanceToGoal = Vector2.Distance(player1Trans.position, _goalLine);
 
-        if (_distanceToBall <= .25f && !_takeShot)
+        if (_distanceToBall <= .25f && !_takeShot)    // player has possession, but cannot take a shot
         {
             _hasBall = true;
         }
-        else
+        else    // otherwise default to player not having possession
         {
             _hasBall = false;
         }
         
         
-        if (!_hasBall && !_takeShot)
+        if (!_hasBall && !_takeShot)    // rotate in figure 8 if player doesn't have possession
         {
             _angle += rotateSpeed * Time.deltaTime;
         
             var offset = new Vector2( Mathf.Sin(_angle) * Mathf.Cos(_angle), Mathf.Sin(_angle)) * radius;
             transform.position = _center + offset;
         }
-        else
+        else    // otherwise translate ball with player until it is in shooting radius
         {
             if (!_takeShot)
             {
@@ -63,12 +65,12 @@ public class ControllerBall : MonoBehaviour
             }
         }
 
-        if (_distanceToGoal < shotRadius)
+        if (_distanceToGoal < shotRadius)    // player has ball and is within shooting radius
         {
             _takeShot = true;
         }
 
-        if (_takeShot)
+        if (_takeShot)    // player takes shot, will utilize randomized exp. func which adds curve to ball and defines a chance of scoring goal based on distance
         {
             transform.position = Vector2.MoveTowards(transform.position, _goalLine, shotSpeed);
         }
