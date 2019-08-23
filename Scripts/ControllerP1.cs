2dc0e14a46e05fbad5e841f6bb1592c5f8c58d6a
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-08-23 15:17:37 -0500

    Update ControllerP1.cs

--- Scripts/ControllerP1.cs
+++ Scripts/ControllerP1.cs
@@ -2,39 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// tracks ball at speed set in Unity engine then shoots when in shooting radius of goal
-// conditionals & booleans need consolidation
+// tracks ball at speed set in Unity then shoots when in shooting radius of goal
+// conditionals and booleans need consolidation
+// defined as controller for 1st player, but will act as foundation for all players
 
 public class ControllerP1 : MonoBehaviour
 {
-    public Transform ballTrans;
+    public Transform ballTrans;    // allows ball position to be shared with player
 
     private bool _hasBall;
     
     public float speed;
-    private Vector2 _goalLine;
+    private Vector2 _goalLine;    // position of goal
     private Vector2 _startingPosition;
     private float _distanceToGoal;
     private float _distanceToBall;
-    public float shotRadius;
+    public float shotRadius;    // allows distance which player will take a shot to be assigned in Unity
     
     private bool _takeShot;
     
     
     void Start()
     {
-        _hasBall = false;
-        _goalLine.x = 0;
+        _hasBall = false;    // defaults to player not having possession
+        _goalLine.x = 0;    // defines position which player should shoot for the goal at
         _goalLine.y = 5.5f;
         
     }
 
     void Update()
     {
-        _distanceToBall = Vector2.Distance(transform.position, ballTrans.position);
+        _distanceToBall = Vector2.Distance(transform.position, ballTrans.position);    // updates distance to ball and goal to allow for player transform
         _distanceToGoal = Vector2.Distance(transform.position, _goalLine);
 
-        if (_distanceToBall <= .25f && !_takeShot)
+        if (_distanceToBall <= .25f && !_takeShot)    // defines radius which allows player to take possession of ball
         {
             _hasBall = true;
         }
@@ -43,21 +44,21 @@ public class ControllerP1 : MonoBehaviour
             _hasBall = false;
         }
 
-        float step = speed * Time.deltaTime;
+        float step = speed * Time.deltaTime;    // defines velocity which player moves
         
-        if (_hasBall && !_takeShot)
+        if (_hasBall && !_takeShot)    // if the player has ball, but is outside shooting radius, move towards goal
         {
             transform.position = Vector2.MoveTowards(transform.position, _goalLine, step);
         }
         else
         {
-            if (!_takeShot)
+            if (!_takeShot)    // otherwise player does not have ball and must move towards it
             {
                 transform.position = Vector2.MoveTowards(transform.position, ballTrans.position, step);
             }
         }
 
-        if (_distanceToGoal < shotRadius)
+        if (_distanceToGoal < shotRadius)    // take shot when within radius
         {
             _takeShot = true;
         }
