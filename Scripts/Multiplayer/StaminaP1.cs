66a6f281a37a7ffbad40cf7ff802945e97596a4f
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-11-02 21:31:40 -0500

    Create StaminaP1.cs

---
+++ Scripts/Multiplayer/StaminaP1.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class StaminaP1 : MonoBehaviour {
+    
+    public Transform playerTrans;
+    private Vector2 lastPosition;
+    private float deltaP;
+    private const int maxStam = 5;
+    private bool increase;
+    
+    [HideInInspector]
+    public float stamina = maxStam;
+
+    void Start() {
+        lastPosition = transform.position;
+    }
+    
+    private float GetStamina() {
+        deltaP = Vector2.Distance(transform.position, lastPosition);
+
+        if (stamina <= -5)
+            increase = true;
+        else if (stamina >= maxStam)
+            increase = false;
+
+        if (increase)
+            stamina += Time.deltaTime;
+        else
+            stamina -= deltaP;
+        
+
+        return stamina;  
+    }
+
+    void Update() {
+        stamina = GetStamina();
+        lastPosition = transform.position;
+    }
+}
+
+
