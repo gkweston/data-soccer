bd85b04f8f76dfb5e66125fd7cf895d0488e66f9
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-11-02 21:30:46 -0500

    Update ControllerP1.cs

--- Scripts/Multiplayer/ControllerP1.cs
+++ Scripts/Multiplayer/ControllerP1.cs
@@ -1,113 +1,80 @@
 using UnityEngine;
 
-public class ControllerP1 : MonoBehaviour
-{
+public class ControllerP1 : MonoBehaviour {
     public Transform ballTrans;
     public float shotRadius = 2f;   
     
     // calculable distances //
-    private float _distanceToGoal;
-    private float _distanceToBall;
+    private float _distanceToGoal, _distanceToBall;
     
     // position vectors //
-    private Vector2 _goalLine;    // position of goal
+    private Vector2 _goalLine;    
     private Vector2 _startingPosition;
     
     // booleans //
-    public bool takeShot;    // reads true if w/i shooting radius
-    private bool _possession;    // read in from ControllerBall.cs
+    public bool takeShot;   
+    private bool _possession;
     private bool _p2Possession;
     
-    // distance based stamina system variables //
-    private Vector2 _lastPosition;
-    private Vector2 _currentPosition;
-    private float _deltaPosition;
+    // Stamina System //
+
+    private StaminaP1 staminaMethod;
     
-    private bool _hasStamina;
-    public float stamina;
-    private float _maxStamina = 5f;
-    private float _speed = 1f;
-    private float _defaultSpeed;
-    private float _speedBoost = 3f;
+    [HideInInspector]
+    public float playerStam;
+    private int speed;
     
     // stall timer //
-    private bool _stall;
+    private bool _stall = false;
     
     
-    void Start()
-    {
-        _possession = false;    // defaults to player not having possession
-        _goalLine.x = 0;    // defines position which player should shoot for the goal at
+    void Start() {
+        _possession = false;   
+        _goalLine.x = 0;    
         _goalLine.y = 5.5f;
-        
-        // <stamina Counter> //
-
-        _defaultSpeed = _speed;
-        stamina = _maxStamina;
-        
-        _lastPosition = transform.position;
-
     }
     
-    void PlayerMovement()
-    {
+    // Implement stall based on challenge here:
+    void PlayerMovement() {
+        /*
+         * Refactor based on Stamina/Challenge updates
+         */
         var position = transform.position;
-        _distanceToBall = Vector2.Distance(position, ballTrans.position);    // updates distance to ball and goal to allow for player transform
+        _distanceToBall = Vector2.Distance(position, ballTrans.position);   
         _distanceToGoal = Vector2.Distance(position, _goalLine);
         
-        var step = _speed * Time.deltaTime;    // defines velocity which player moves
+        // Stamina based speed:
+        playerStam = staminaMethod.stamina;
+        speed = playerStam > 0 ? 2 : 1;
+        var step = speed * Time.deltaTime;    // movement velocity
 
-        if (_possession)
-        {
-            if (_distanceToGoal <= shotRadius)
-            {
+        if (_possession) {
+            if (_distanceToGoal <= shotRadius) {
                 takeShot = true;
             }
-            else
-            {
+            else {
                 if (_stall) return;
                 transform.position = Vector2.MoveTowards(position, _goalLine, step);
             }
         }
-        else
-        {
+        else {
             if (_stall) return;
             transform.position = Vector2.MoveTowards(position, ballTrans.position, step);
         }
     }
 
-    public void Stamina()
-    {
-        _deltaPosition = Vector2.Distance(transform.position, _lastPosition);
-        
-        if (stamina >= _maxStamina) _hasStamina = true;
-        if (stamina <= 0) _hasStamina = false;
-
-        if (_hasStamina)
-        {
-            stamina -= _deltaPosition;
-            _speed = _speedBoost;
-        }
-
-        if (!_hasStamina)
-        {
-            stamina += Time.deltaTime;
-            _speed = _defaultSpeed;
-        }
-        
-        _lastPosition = transform.position;
+    void Stall() {
+        // stall after lost challenge, secondary to knockback method
     }
 
-    void Update()
-    {
-        _stall = FindObjectOfType<ControllerBall>().p1Stall;
-        _possession = FindObjectOfType<ControllerBall>().p1Possession;    // test for expensive invocation
+    void Knockback() {
+        // knockback after lost challenge -> refer above
+    }
+
+    void FixedUpdate() {
+        //_stall = GetComponent<ControllerBall>().p1Stall;
+        _possession = GetComponent<ControllerBall>().p1Possession;
 
         PlayerMovement();
-        Stamina();
-        
-        // <Debug> //
-
-
     }
 }
