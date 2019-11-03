4da78b48768ac478b299696a537067f67236dc44
Garrett Weston <51564201+gkweston@users.noreply.github.com>
2019-11-02 21:31:10 -0500

    Update ControllerP2.cs

--- Scripts/Multiplayer/ControllerP2.cs
+++ Scripts/Multiplayer/ControllerP2.cs
@@ -1,79 +1,70 @@
 using UnityEngine;
 
-public class ControllerP2 : MonoBehaviour
-{
-    /*
-     * In simultaneous development with ControllerP1 as some functions generalize between the two
-     */
-    
+public class ControllerP2 : MonoBehaviour {
     public Transform ballTrans;
-    public float shotRadius = 2f;
+    public float shotRadius = 2f;   
     
-    private bool _possession;
-    public bool takeShot;
-
-    private float _distanceToBall;
-    private float _distanceToGoal;
+    // calculable distances //
+    private float _distanceToGoal, _distanceToBall;
     
-    private float _speed = 1f;
-    
-    private Vector2 _goalLine;
+    // position vectors //
+    private Vector2 _goalLine;    
     private Vector2 _startingPosition;
+    
+    // booleans //
+    public bool takeShot;    
+    private bool _possession;    
+    private bool _p2Possession;
+    
+    // Stamina System //
 
-    public float stamina;
-
-    private bool _stall;
+    //private Stamina staminaMethod;
+    
+    [HideInInspector]
+    public float playerStam;
+    private int speed;
+    
+    // stall timer //
+    private bool _stall = false;
     
     
-    void Start()
-    {
-
-        _possession = false;    // defaults to player not having possession before Finding possP2 object
-        
-        _goalLine.x = 0f;
-        _goalLine.y = -5.5f;
-
-        stamina = 50f;
-
+    void Start() {
+        _possession = false;    // defaults to player not having possession
+        _goalLine.x = 0;    // defines position which player should shoot for the goal at
+        _goalLine.y = 5.5f;
     }
-
-    void PlayerMovement()
-    {
+    
+    void PlayerMovement() {
+        /*
+         * Refactor based on Stamina/Challenge updates
+         */
         var position = transform.position;
         _distanceToBall = Vector2.Distance(position, ballTrans.position);    // updates distance to ball and goal to allow for player transform
         _distanceToGoal = Vector2.Distance(position, _goalLine);
         
-        var step = _speed * Time.deltaTime;    // defines velocity which player moves
+        // Stamina based speed:
+        //playerStam = staminaMethod.stamina;
+        Debug.Log(playerStam);
+        speed = playerStam > 0 ? 2 : 1;
+        var step = speed * Time.deltaTime;    // defines velocity which player moves
 
-        if (_possession)
-        {
-            if (_distanceToGoal <= shotRadius)
-            {
+        if (_possession) {
+            if (_distanceToGoal <= shotRadius) {
                 takeShot = true;
             }
-            else
-            {
+            else {
                 if (_stall) return;
                 transform.position = Vector2.MoveTowards(position, _goalLine, step);
             }
         }
-        else
-        {
+        else {
             if (_stall) return;
             transform.position = Vector2.MoveTowards(position, ballTrans.position, step);
         }
     }
-    
-    void Update()
-    {
-        _stall = FindObjectOfType<ControllerBall>().p2Stall;
-        _possession = FindObjectOfType<ControllerBall>().p2Possession;    // check for expensive invocation in testing
-        
+
+    void FixedUpdate() {
+        _possession = FindObjectOfType<ControllerBall>().p2Possession;    // test for expensive invocation
         PlayerMovement();
-        
-        // <Debug> //
-
-
-        
     }
 }
